9c812c78500a5f773d71cd2de03df71f966130ca
Glenn Rempe <glenn@rempe.us>
2015-07-20 10:34:32 -0700

    Added ability to generate Bitcoin 'brain' wallets. Significant re-factor.

--- js/main.js
+++ js/main.js
@@ -1,36 +1,61 @@
-function computePassword(basePassPhrase, usernameOrAppname, siteUri, version, extraSalt) {
+// Setup Bitcore Bitcoin Library
+var bitcore = require('bitcore');
+var PrivateKey = bitcore.PrivateKey;
+var PublicKey = bitcore.PublicKey;
+var Address = bitcore.Address;
+var Networks = bitcore.Networks;
 
-  // trim leading and trailing whitespace from all fields
-  basePassPhrase = $.trim(basePassPhrase);
-  usernameOrAppname = $.trim(usernameOrAppname);
-  siteUri = $.trim(siteUri);
-  version = $.trim(version);
-  extraSalt = $.trim(extraSalt);
+// takes a 'key' in Hex format
+function generateBitcoinKeyDataFromKey(key) {
+    var privateKey = new PrivateKey(key);
+    var privateKeyWIF = privateKey.toWIF();
+    var publicKey = privateKey.toPublicKey();
+    var address = publicKey.toAddress(Networks.livenet);
 
-  // normalize the non-secret params
-  // some browsers, especially on mobile, will auto-capitalize
-  // which will change the password results.
-  usernameOrAppname = usernameOrAppname.toLowerCase();
-  siteUri = siteUri.toLowerCase();
-  version = parseInt(version);
+    return {privateKey: privateKey, privateKeyWIF: privateKeyWIF, publicKey: publicKey, address: address};
+}
+
+function hasValidArgsForMode(args) {
+    if (args.mode === "web" && args.passphrase.length > 0 && args.webUsername.length > 0 && args.webDomain.length > 0 && args.version > 0) {
+        return true;
+    } else if (args.mode === "app" && args.passphrase.length > 0 && args.appName.length > 0 && args.version > 0) {
+        return true;
+    } else if (args.mode === "btc" && args.passphrase.length > 0) {
+        return true;
+    } else {
+        return false;
+    }
+
+}
+function processPassphrase(args) {
+
+    var usernameOrAppname = "";
+
+    if (args.webUsername !== "") {
+        usernameOrAppname = args.webUsername;
+    }
+
+    if (args.appName !== "") {
+        usernameOrAppname = args.appName;
+    }
 
   // Estimate base password entropy with ZXCVBN
   // https://blogs.dropbox.com/tech/2012/04/zxcvbn-realistic-password-strength-estimation/
   // https://dl.dropboxusercontent.com/u/209/zxcvbn/test/index.html
-  var zxcvbnPassphrase = zxcvbn(basePassPhrase);
+  var zxcvbnPassphrase = zxcvbn(args.passphrase);
 
-  var host = extractHostFromUri(siteUri);
+  var host = extractHostFromUri(args.webDomain);
 
-  if (zxcvbnPassphrase.score >= 4 && usernameOrAppname.length >= 1 && host && version >= 1) {
+  if (zxcvbnPassphrase.score >= 4  && hasValidArgsForMode(args)) {
 
       // Generate a master key w/ HMAC-SHA-256, from passphrase and username
-      var passPhraseUint8 = nacl.util.decodeUTF8(basePassPhrase);                          // Byte Array
+      var passPhraseUint8 = nacl.util.decodeUTF8(args.passphrase);                         // Byte Array
       var usernameOrAppnameUint8 = nacl.util.decodeUTF8(usernameOrAppname);                // Byte Array
       var usernameOrAppnameHashedUint8 = nacl.hash(usernameOrAppnameUint8);                // SHA-512, 64 Bytes
       var masterKeyUint8 = nacl.auth.full(passPhraseUint8, usernameOrAppnameHashedUint8);  // HMAC-SHA-512, 64 Bytes
 
       // Construct a salt for key derivation and pass it through SHA-512
-      var paramsCombined = usernameOrAppname + '@' + host + ':v' + version + ':' + extraSalt; // String
+      var paramsCombined = usernameOrAppname + '@' + host + ':v' + args.version + ':' + args.salt; // String
       var paramsCombinedUint8 = nacl.util.decodeUTF8(paramsCombined);                         // Byte Array
       var kdSaltUint8 = nacl.hash(paramsCombinedUint8);                                       // SHA-512, 64 Bytes
 
@@ -40,7 +65,7 @@ function computePassword(basePassPhrase, usernameOrAppname, siteUri, version, ex
       // https://stackoverflow.com/questions/11126315/what-are-optimal-scrypt-work-factors
       //
       // masterKeyUint8 = h(passphrase, usernameOrAppname)
-      // kdSaltUint8 = h(usernameOrAppname | host | version | extraSalt)
+      // kdSaltUint8 = h(usernameOrAppname | host | version | salt)
       // L bytes of derived key material from a password passwd and a salt salt
       // N, which must be a power of two, which will set the overall difficulty
       //   of the computation. The scrypt paper uses 2^14 = 16384 for interactive
@@ -57,37 +82,48 @@ function computePassword(basePassPhrase, usernameOrAppname, siteUri, version, ex
       var N = 16384; // 2^14 : 128×16384×8 = 16,777,216 bytes = 16 MB RAM, 16384 iterations.
       var r = 8;     //
       var p = 1;     //
-      var L = 32;    // Output Bytes
+      var L = 64;    // Output Bytes
       var kdBytesUint8 = scrypt.crypto_scrypt(masterKeyUint8, kdSaltUint8, N, r, p, L);
+
+      // split the key derived bytes. Use half of the 64 Bytes for password
+      // generation, and the other half for Bitcoin keypair.
+      kdBytesUint8ForPass = kdBytesUint8.subarray(0,32);
+      kdBytesUint8ForBTC = kdBytesUint8.subarray(32, 64);
 
       // calculate a symbol from last byte to ensure every generated password
       // has at least one symbol since the Base64 output doesn't guarantee.
-      var lastByte = kdBytesUint8[31];
+      var lastByte = kdBytesUint8ForPass[31];
       var symbols = ['!', '@', '#', '$', '%', '?', '&', '*', '+', '-'];
       var symbolIndexForByte = lastByte % 10;
       var chosenSymbol = symbols[symbolIndexForByte];
 
       // calculate a number from second to last byte to ensure every generated
       // password has at least one number since the Base64 output doesn't guarantee.
-      var secondToLastByte = kdBytesUint8[30];
+      var secondToLastByte = kdBytesUint8ForPass[30];
       var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
       var numberIndexForByte = secondToLastByte % 10;
       var chosenNumber = numbers[numberIndexForByte];
 
       // Convert the key derivation function output to Base 64 (does not include
       // extra number or symbol yet)
-      var kdBytesBase64 = nacl.util.encodeBase64(kdBytesUint8); // Base 64 String
+      var kdBytesBase64ForPass = nacl.util.encodeBase64(kdBytesUint8ForPass); // Base 64 String for password
+      var kdBytesHexForBTC = scrypt.to_hex(kdBytesUint8ForBTC);              // Hex String for Bitcoin Keypair
 
       // Take only the first N bytes of the Base 64 encoded password as the final password.
       // Append a deterministically chosen symbol and number to ensure meeting most password requirements
-      var password = kdBytesBase64.substring(0, 18) + chosenSymbol + chosenNumber; // Partial Base 64 String
+      var password = kdBytesBase64ForPass.substring(0, 18) + chosenSymbol + chosenNumber; // Partial Base 64 String
+
+      var bitcoinKeyData = {};
+      if (isBtcMode()) {
+          bitcoinKeyData = generateBitcoinKeyDataFromKey(kdBytesHexForBTC);
+      }
 
       // Calc the estimated entropy of the final encoded password.
       var zxcvbnPassword = zxcvbn(password);
 
       return {usernameOrAppname: usernameOrAppname, host: host, password: password,
-              passphraseEntropy: zxcvbnPassphrase.entropy,
-              passwordEntropy: zxcvbnPassword.entropy};
+              version: args.version, salt: args.salt, passphraseEntropy: zxcvbnPassphrase.entropy,
+              passwordEntropy: zxcvbnPassword.entropy, bitcoinKeyData: bitcoinKeyData};
   } else {
       return null;
   }
@@ -112,14 +148,70 @@ function extractHostFromUri(uri) {
     }
 }
 
-function computeSaltInWords(usernameOrAppname, domain, version, extraSalt) {
-  if (usernameOrAppname && domain && version >= 1 && extraSalt.length >= 1) {
-    return usernameOrAppname + '@' + domain + ':v' + version + ':*****';
-  } else if (usernameOrAppname && domain && version >= 1 && extraSalt === '') {
-    return usernameOrAppname + '@' + domain + ':v' + version ;
-  } else {
-    return null;
-  }
+// FIXME : The output of this should be something you can write down to contain all of the settings used for this password.
+// function computeSaltInWords(usernameOrAppname, domain, version, salt) {
+//   if (usernameOrAppname && domain && version >= 1 && salt.length >= 1) {
+//     return usernameOrAppname + '@' + domain + ':v' + version + ':*****';
+// } else if (usernameOrAppname && domain && version >= 1 && salt === '') {
+//     return usernameOrAppname + '@' + domain + ':v' + version ;
+//   } else {
+//     return null;
+//   }
+// }
+
+function clearInputs() {
+    $("#keyPassphraseInput").val("");
+    $("#keyPassphraseInput").pwstrength("forceUpdate");
+    $("#tokenInput").val("");
+    $("#webUsernameInput").val("");
+    $("#webDomainInput").val("");
+    $("#appNameInput").val("");
+    $("#versionInput").val(1);
+}
+
+function setWebMode() {
+    $("#keyPassphraseInputGroup").slideDown();
+    $("#tokenInputGroup").slideDown();
+    $("#webUsernameInputGroup").slideDown();
+    $("#webDomainInputGroup").slideDown();
+    $("#appNameInputGroup").slideUp();
+    $("#versionInputGroup").slideDown();
+}
+
+function setAppMode() {
+    $("#keyPassphraseInputGroup").slideDown();
+    $("#tokenInputGroup").slideDown();
+    $("#webUsernameInputGroup").slideUp();
+    $("#webDomainInputGroup").slideUp();
+    $("#appNameInputGroup").slideDown();
+    $("#versionInputGroup").slideDown();
+}
+
+function setBtcMode() {
+    $("#keyPassphraseInputGroup").slideDown();
+    $("#tokenInputGroup").slideDown();
+    $("#webUsernameInputGroup").slideUp();
+    $("#webDomainInputGroup").slideUp();
+    $("#appNameInputGroup").slideUp();
+    $("#versionInputGroup").slideUp();
+}
+
+function isWebMode() {
+    if ($("#webModeButton").hasClass("active")) {
+        return true;
+    }
+}
+
+function isAppMode() {
+    if ($("#appModeButton").hasClass("active")) {
+        return true;
+    }
+}
+
+function isBtcMode() {
+    if ($("#btcModeButton").hasClass("active")) {
+        return true;
+    }
 }
 
 // https://stackoverflow.com/questions/1909441/jquery-keyup-delay
@@ -131,39 +223,109 @@ var delay = (function(){
   };
 })();
 
-function updatePasswordOutputContainer() {
-
-    // delay execution for 500ms after keyups stop.
+function updateOutputContainers() {
+    // delay execution for a number of milliseconds after keyups stop.
     delay(function(){
         // force password strength update
         $("#keyPassphraseInput").pwstrength("forceUpdate");
 
-        var basePassphrase = $('#keyPassphraseInput').val();
-        var usernameOrAppname = $('#usernameOrAppnameInput').val();
-        var siteUri = $('#domainInput').val();
-        var version = $('#versionInput').val();
-        var extraSalt = $('#tokenInput').val();
+        var passphrase = $.trim($('#keyPassphraseInput').val());
+        var salt = $.trim($('#tokenInput').val());
+        var webUsername = $.trim($('#webUsernameInput').val()).toLowerCase();
+        var webDomain = $.trim($('#webDomainInput').val()).toLowerCase();
+        var appName = $.trim($('#appNameInput').val()).toLowerCase();
+        var version = parseInt($.trim($('#versionInput').val()));
 
-        var passwordObj = computePassword(basePassphrase, usernameOrAppname, siteUri, version, extraSalt);
+        var mode;
+        if (isWebMode()) {
+            mode = "web";
+        } else if (isAppMode()) {
+            mode = "app";
+        } else if (isBtcMode()) {
+            mode = "btc";
+        }
 
-        if (passwordObj && passwordObj.usernameOrAppname && passwordObj.host && passwordObj.password) {
-            $("#passwordOutput").text(passwordObj.password);
-            $("#passwordEntropy").text(passwordObj.passwordEntropy);
-            $("#sanitizedSaltInWords").text(computeSaltInWords(passwordObj.usernameOrAppname, passwordObj.host, version, extraSalt));
-            $("#passwordOutputContainer").slideDown();
-        } else {
-            $("#passwordOutput").text('');
-            $("#sanitizedSaltInWords").text('');
-            $("#passwordOutputContainer").slideUp();
+        var processPassphraseArgs = {mode: mode, passphrase: passphrase, salt: salt, webUsername: webUsername, webDomain: webDomain, appName: appName, version: version};
+        var securityObj = processPassphrase(processPassphraseArgs);
+
+        if (isWebMode()) {
+            updatePasswordOutputContainer(securityObj);
+            updateBitcoinOutputContainer({});
+        } else if (isAppMode()) {
+            updatePasswordOutputContainer(securityObj);
+            updateBitcoinOutputContainer({});
+        } else if (isBtcMode()) {
+            updatePasswordOutputContainer({});
+            updateBitcoinOutputContainer(securityObj);
         }
     }, 400 );
+}
 
+function updatePasswordOutputContainer(securityObj) {
+    if (isWebMode() && securityObj && securityObj.usernameOrAppname && securityObj.host && securityObj.password) { // web mode
+        $("#passwordOutput").text(securityObj.password);
+        $("#passwordEntropy").text(securityObj.passwordEntropy);
+        // $("#sanitizedSaltInWords").text(computeSaltInWords(securityObj.usernameOrAppname, securityObj.host, securityObj.version, securityObj.salt));
+        $("#passwordOutputContainer").slideDown();
+    } else if (isAppMode() && securityObj && securityObj.usernameOrAppname && securityObj.password) { // app mode
+        $("#passwordOutput").text(securityObj.password);
+        $("#passwordEntropy").text(securityObj.passwordEntropy);
+        // $("#sanitizedSaltInWords").text(computeSaltInWords(securityObj.usernameOrAppname, securityObj.host, securityObj.version, securityObj.salt));
+        $("#passwordOutputContainer").slideDown();
+    } else { // btc mode
+        $("#passwordOutput").text('');
+        $("#sanitizedSaltInWords").text('');
+        $("#passwordOutputContainer").slideUp();
+    }
+}
+
+function updateBitcoinOutputContainer(securityObj) {
+    if (isBtcMode() && securityObj && securityObj.bitcoinKeyData) {
+        $("#bitcoinPrivateKey").text(securityObj.bitcoinKeyData.privateKey);
+        $("#bitcoinPrivateKeyWIF").text(securityObj.bitcoinKeyData.privateKeyWIF);
+        $("#bitcoinPublicKey").text(securityObj.bitcoinKeyData.publicKey);
+        $("#bitcoinAddress").text(securityObj.bitcoinKeyData.address);
+        $('#qrcode').empty().qrcode({width: 148,height: 148, typeNumber: 5, text: "bitcoin:" + securityObj.bitcoinKeyData.address});
+        $("#qrcodeAddress").text("bitcoin:" + securityObj.bitcoinKeyData.address);
+        $("#bitcoinOutputContainer").slideDown();
+    } else {
+        $("#bitcoinPrivateKey").empty();
+        $("#bitcoinPrivateKeyWIF").empty();
+        $("#bitcoinPublicKey").empty();
+        $("#bitcoinAddress").empty();
+        $('#qrcode').empty();
+        $("#qrcodeAddress").empty();
+        $("#bitcoinOutputContainer").slideUp();
+    }
 }
 
 $(document).ready(function () {
     'use strict';
 
+    $("#inputForm").hide();
     $("#passwordOutputContainer").hide();
+    $("#bitcoinOutputContainer").hide();
+
+    // a click on any mode selector button
+    $(".modeSelector").click(function() {
+        $("#inputForm").slideDown();
+        clearInputs();
+        updateOutputContainers();
+        $(".modeSelector").removeClass("active");
+        $(this).addClass("active");
+    });
+
+    $("#webModeButton").click(function() {
+        setWebMode();
+    });
+
+    $("#appModeButton").click(function() {
+        setAppMode();
+    });
+
+    $("#btcModeButton").click(function() {
+        setBtcMode();
+    });
 
     // Configure the password strength meter.
     // https://github.com/ablanco/jquery.pwstrength.bootstrap
@@ -174,27 +336,27 @@ $(document).ready(function () {
         common: {
             zxcvbn: true,
             zxcvbnTerms: ['secret', 'password'],
-            userInputs: ['#usernameInput', '#domainInput', '#tokenInput']
+            userInputs: ['#webUsernameInput', '#webDomainInput', '#appNameInput', '#tokenInput']
         }
     });
 
     // All form control elements keyup
     $('.form-control').on('keyup', function () {
-        updatePasswordOutputContainer();
+        updateOutputContainers();
     });
 
     // Special handling for the HTML5 scroll arrows for a number field
     // https://stackoverflow.com/questions/5669207/html5-event-listener-for-number-input-scroll-chrome-only
     $('#versionInput').click(function(){
-        updatePasswordOutputContainer();
+        updateOutputContainers();
     });
 
     $('#versionInput').change(function(){
-        updatePasswordOutputContainer();
+        updateOutputContainers();
     });
 
     $('#versionInput').keypress(function(){
-        updatePasswordOutputContainer();
+        updateOutputContainers();
     });
 
 });
